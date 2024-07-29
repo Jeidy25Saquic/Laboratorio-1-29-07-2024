fbab987b8f59ff756dae4177eb3c664b9dd5309a
Jennifer Saquic <jenntecum2001@gmail.com>
2024-07-29 09:07:10 -0600

    Arreglar exepciones

--- Tienda/Tienda/Program.cs
+++ Tienda/Tienda/Program.cs
@@ -7,6 +7,7 @@
     {
         
         bool continuar = true;
+        int opcion = 0;
         do
         {
             try
@@ -15,11 +16,20 @@
                 Console.WriteLine("1. Ingresar producto");
                 Console.WriteLine("2.Conteo de productos");
                 Console.WriteLine("3. total de la compra");
-                int opcion= Convert.ToInt32(Console.ReadLine());
+                opcion = Convert.ToInt32(Console.ReadLine());
+                while (opcion < 1 || opcion > 4)
+                {
+                    Console.WriteLine("Valor no esta en el rango...")
+                        ; Console.WriteLine("Ingrese un valor: ");
+                    opcion = Convert.ToInt32(Console.ReadLine());
+                } 
                 switch(opcion)
                 {
                     case 1:
                         IngresarProducto();
+                        break;
+                    case 2:
+                        ConteoProductos();
                         break;
 
                         
@@ -59,6 +69,12 @@
             Console.WriteLine("Ha ocurrido un error "+ ex.Message);
         
         }
+
+    }
+
+    static void ConteoProductos()
+    {
+        Console.WriteLine("La cantidad de productos ingresados es: " + productos.Count);
 
     }
 
