d79d1bba2ba95b99de09d3feda77ff9a7ea0b111
Jennifer Saquic <jenntecum2001@gmail.com>
2024-07-31 06:25:20 -0600

    No subi los cambios

---
+++ Segundo Ejercicio/Segundo Ejercicio/Producto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Segundo_Ejercicio
+{
+    public class Producto
+    {
+        string Name {  get; set; }
+        int Precio { get; set; }
+        int Stock {  get; set; }
+         
+        public Producto(string nombre, int precio, int stock) {
+
+            Name = nombre;
+            Precio = precio;
+            Stock = stock;
+        }  
+        
+       
+        public void Consultar(string nombre)
+        {
+            if(nombre!= Name)
+            {
+                Console.WriteLine("No existe ese producto");
+            }
+            else
+            {
+                Console.WriteLine("Nombre del poroducto"+ Name);
+                Console.WriteLine("Precio" + Precio);
+                Console.WriteLine("Stock: " + Stock);
+            }
+        }
+    }
+}
--- Segundo Ejercicio/Segundo Ejercicio/Program.cs
+++ Segundo Ejercicio/Segundo Ejercicio/Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Segundo_Ejercicio;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+class Program
+{
+
+
+    static void Main(string[] args)
+    {
+
+
+        bool continuar = true;
+        int opcion = 0, precio=0, stock=0;
+        string nombreProducto="";
+       Producto producto = new Producto(nombreProducto, precio, stock);
+        do
+        {
+            try
+            {
+                Console.WriteLine(" Tienda");
+                Console.WriteLine("1. Ingresar producto");
+                Console.WriteLine("2.Mostrar pRODUCTO ");
+           
+                opcion = Convert.ToInt32(Console.ReadLine());
+                while (opcion < 1 || opcion > 4)
+                {
+                    Console.WriteLine("Valor no esta en el rango...")
+                        ; Console.WriteLine("Ingrese un valor: ");
+                    opcion = Convert.ToInt32(Console.ReadLine());
+                }
+                switch (opcion)
+                {
+                    case 1:
+
+                        Console.WriteLine("Nombre del poroducto");
+                        nombreProducto = Console.ReadLine();
+                        Console.WriteLine("Precio");
+                        precio = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Stock: ");
+                        stock = Convert.ToInt32(Console.ReadLine());
+                       
+
+
+                        break;
+                    case 2:
+                        Console.WriteLine("Ingres el nombre del producto que busca ");
+                        string nombre=Console.ReadLine();
+                        producto.Consultar(nombre);
+                        break;
+                    case 3:
+                        break;
+
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ha ocurrido un error... " + ex.Message);
+
+            }
+
+
+        } while (continuar == true);
+
+
+    }
+}
